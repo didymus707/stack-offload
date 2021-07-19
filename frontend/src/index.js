742c7803be6d16de50d37ac787c76d37dd0ae769
pavinduLakshan <174084F@uom.lk>
2021-07-19 10:36:49 +0530

    install and oconfigure chakra ui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import {theme} from "components/ThemeProvider"
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from "@chakra-ui/react"
+import "@fontsource/raleway/400.css"
+import "@fontsource/open-sans/700.css"
 
 ReactDOM.render(
   <React.StrictMode>
+    <ChakraProvider theme={theme}>
     <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
